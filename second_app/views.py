0acbadd760e6fb293fa266424d08c6e424ad537f
srivarshithagatta <srivarshithagatta@gmail.com>
2024-10-22 17:50:15 +0530

    initial commit

---
+++ second_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+def home(request): 
+    return render(request,'home.html',{'name':'varshi'}) 
+# Create your views here.
+def add(request): 
+    val1=int(request.POST['num1']) 
+    val2=int(request.POST['num2']) 
+    val3=val1+val2 
+    return render(request,'result.html',{'result':val3})
